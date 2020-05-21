23f24a335395021a2d6749bf46458a7a6d1287aa
Glitch (youthful-wiggly-tamarind) <none>
2020-05-21 19:37:30 +0000

    🏄💽 Checkpoint ./wit.js:16216520/545

--- wit.js
+++ wit.js
@@ -8,11 +8,7 @@
 // We assume you have:
 // * a Wit.ai bot setup (https://wit.ai/docs/quickstart)
 // * a Messenger Platform setup (https://developers.facebook.com/docs/messenger-platform/quickstart)
-// You need to `npm install` the following dependencies: body-parser, express, node-fetch.
 //
-// 1. npm install body-parser express node-fetch
-// 2. Download and install ngrok from https://ngrok.com/download
-// 3. ./ngrok http 8445
 // 4. WIT_TOKEN=your_access_token FB_APP_SECRET=your_app_secret FB_PAGE_TOKEN=your_page_token node examples/messenger.js
 // 5. Subscribe your page to the Webhooks using verify_token and `https://<your_ngrok_io>/webhook` as callback URL.
 // 6. Talk to your bot on Messenger!
@@ -162,17 +158,16 @@ app.post('/webhook', (req, res) => {
             // Let's run /message on the text to extract some entities, intents and traits
             wit.message(text).then(({entities, intents, traits}) => {
               // You can customize your response using these
-              console.log("log intents:");
-              console.log(intents);
-              console.log("- - - ");
-     
-              console.log("log entities:");
-              console.log(entities);
-              console.log("- - ");
-              console.log(entities.notable_person);
-              console.log("- - ");
-              console.log(entities.somePhrase);
-              console.log("- - - ");
+              // console.log("log intents:");
+              // console.log(intents);
+              // console.log("- - - ");
+              // console.log("log entities:");
+              // console.log(entities);
+              // console.log("- - ");
+              // console.log(entities.notable_person);
+              // console.log("- - ");
+              // console.log(entities.somePhrase);
+              // console.log("- - - ");
               
               var person = personFromEntities(entities);
               if (person == null){
