d35be805fa86a53c201d207a1e2f02076be12405
Glitch (youthful-wiggly-tamarind) <none>
2020-05-26 17:10:30 +0000

    🚞🖼 Checkpoint ./app.js:16216520/12754 ./wit_handler.js:16216520/13806 ./package.json:16216520/523

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
  */
 
-'use strict';
+"use strict";
 
 // Messenger API integration example
 // We assume you have:
@@ -13,21 +13,21 @@
 // 5. Subscribe your page to the Webhooks using verify_token and `https://<your_ngrok_io>/webhook` as callback URL.
 // 6. Talk to your bot on Messenger!
 
-const bodyParser = require('body-parser');
-const crypto = require('crypto');
-const express = require('express');
-const fetch = require('node-fetch');
-const handler = require('./wit_handler');
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
+const express = require("express");
+const fetch = require("node-fetch");
+const handler = require("./wit_handler");
 
 let Wit = null;
 let log = null;
 try {
   // if running from repo
-  Wit = require('../').Wit;
-  log = require('../').log;
+  Wit = require("../").Wit;
+  log = require("../").log;
 } catch (e) {
-  Wit = require('node-wit').Wit;
-  log = require('node-wit').log;
+  Wit = require("node-wit").Wit;
+  log = require("node-wit").log;
 }
 
 // Webserver parameter
@@ -38,17 +38,13 @@ const WIT_TOKEN = process.env.WIT_TOKEN;
 
 // Messenger API parameters
 const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
-if (!FB_PAGE_TOKEN) { throw new Error('missing FB_PAGE_TOKEN') }
+if (!FB_PAGE_TOKEN) {
+  throw new Error("missing FB_PAGE_TOKEN");
+}
 const FB_APP_SECRET = process.env.FB_APP_SECRET;
-if (!FB_APP_SECRET) { throw new Error('missing FB_APP_SECRET') }
-
-// let FB_VERIFY_TOKEN = null;
-// crypto.randomBytes(8, (err, buff) => {
-//   if (err) throw err;
-//   FB_VERIFY_TOKEN = buff.toString('hex');
-//   console.log(`/webhook will accept the Verify Token "${FB_VERIFY_TOKEN}"`);
-// });
-
+if (!FB_APP_SECRET) {
+  throw new Error("missing FB_APP_SECRET");
+}
 
 // ----------------------------------------------------------------------------
 // Messenger API specific code
@@ -59,21 +55,21 @@ if (!FB_APP_SECRET) { throw new Error('missing FB_APP_SECRET') }
 const fbMessage = (id, text) => {
   const body = JSON.stringify({
     recipient: { id },
-    message: { text },
+    message: { text }
   });
-  const qs = 'access_token=' + encodeURIComponent(FB_PAGE_TOKEN);
-  return fetch('https://graph.facebook.com/me/messages?' + qs, {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body,
+  const qs = "access_token=" + encodeURIComponent(FB_PAGE_TOKEN);
+  return fetch("https://graph.facebook.com/me/messages?" + qs, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body
   })
-  .then(rsp => rsp.json())
-  .then(json => {
-    if (json.error && json.error.message) {
-      throw new Error(json.error.message);
-    }
-    return json;
-  });
+    .then(rsp => rsp.json())
+    .then(json => {
+      if (json.error && json.error.message) {
+        throw new Error(json.error.message);
+      }
+      return json;
+    });
 };
 
 // ----------------------------------------------------------------------------
@@ -84,7 +80,7 @@ const fbMessage = (id, text) => {
 // sessionId -> {fbid: facebookUserId, context: sessionState}
 const sessions = {};
 
-const findOrCreateSession = (fbid) => {
+const findOrCreateSession = fbid => {
   let sessionId;
   // Let's see if we already have a session for the user fbid
   Object.keys(sessions).forEach(k => {
@@ -96,7 +92,7 @@ const findOrCreateSession = (fbid) => {
   if (!sessionId) {
     // No session found for user fbid, let's create a new one
     sessionId = new Date().toISOString();
-    sessions[sessionId] = {fbid: fbid, context: {}};
+    sessions[sessionId] = { fbid: fbid, context: {} };
   }
   return sessionId;
 };
@@ -109,8 +105,8 @@ const wit = new Wit({
 
 // Starting our webserver and putting it all together
 const app = express();
-app.use(({method, url}, rsp, next) => {
-  rsp.on('finish', () => {
+app.use(({ method, url }, rsp, next) => {
+  rsp.on("finish", () => {
     console.log(`${rsp.statusCode} ${method} ${url}`);
   });
   next();
@@ -118,23 +114,25 @@ app.use(({method, url}, rsp, next) => {
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 
 // Webhook setup
-app.get('/webhook', (req, res) => {
-  if (req.query['hub.mode'] === 'subscribe' &&
-    req.query['hub.verify_token'] === process.env.FB_VERIFY_TOKEN) {
-    res.send(req.query['hub.challenge']);
+app.get("/webhook", (req, res) => {
+  if (
+    req.query["hub.mode"] === "subscribe" &&
+    req.query["hub.verify_token"] === process.env.FB_VERIFY_TOKEN
+  ) {
+    res.send(req.query["hub.challenge"]);
   } else {
     res.sendStatus(400);
   }
 });
 
 // Message handler
-app.post('/webhook', (req, res) => {
+app.post("/webhook", (req, res) => {
   // Parse the Messenger payload
   // See the Webhook reference
   // https://developers.facebook.com/docs/messenger-platform/webhook-reference
   const data = req.body;
 
-  if (data.object === 'page') {
+  if (data.object === "page") {
     data.entry.forEach(entry => {
       entry.messaging.forEach(event => {
         if (event.message && !event.message.is_echo) {
@@ -147,36 +145,39 @@ app.post('/webhook', (req, res) => {
           // const sessionId = findOrCreateSession(sender);
 
           // We retrieve the message content
-          const {text, attachments} = event.message;
+          const { text, attachments } = event.message;
 
           if (attachments) {
             // We received an attachment
             // Let's reply with an automatic message
-            fbMessage(sender, 'Sorry I can only process text messages for now.')
-            .catch(console.error);
+            fbMessage(
+              sender,
+              "Sorry I can only process text messages for now."
+            ).catch(console.error);
           } else if (text) {
             // We received a text message
             // Let's run /message on the text to extract some entities, intents and traits
-            wit.message(text)
-              .then((res) => handler.responseFromWit(res))
-              .then((msg) => {
-              fbMessage(sender, msg);
-            })
-            .catch((err) => {
-              console.error('Oops! Got an error from Wit: ', err.stack || err);
-            })
+            wit
+              .message(text)
+              .then(res => handler.responseFromWit(res))
+              .then(msg => {
+                fbMessage(sender, msg);
+              })
+              .catch(err => {
+                console.error(
+                  "Oops! Got an error from Wit: ",
+                  err.stack || err
+                );
+              });
           }
         } else {
-          console.log('received event', JSON.stringify(event));
+          console.log("received event", JSON.stringify(event));
         }
       });
     });
   }
   res.sendStatus(200);
 });
-
-
-
 
 /*
  * Verify that the callback came from Facebook. Using the App Secret from
@@ -195,13 +196,14 @@ function verifyRequestSignature(req, res, buf) {
     // error.
     console.error("Couldn't validate the signature.");
   } else {
-    var elements = signature.split('=');
+    var elements = signature.split("=");
     var method = elements[0];
     var signatureHash = elements[1];
 
-    var expectedHash = crypto.createHmac('sha1', FB_APP_SECRET)
-                        .update(buf)
-                        .digest('hex');
+    var expectedHash = crypto
+      .createHmac("sha1", FB_APP_SECRET)
+      .update(buf)
+      .digest("hex");
 
     if (signatureHash != expectedHash) {
       throw new Error("Couldn't validate the request signature.");
@@ -210,4 +212,4 @@ function verifyRequestSignature(req, res, buf) {
 }
 
 app.listen(PORT);
-console.log('Listening on :' + PORT + '...');
+console.log("Listening on :" + PORT + "...");
